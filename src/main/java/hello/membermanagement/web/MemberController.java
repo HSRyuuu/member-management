05e4fc41393c94d01dcc781a08d41ec41f943abe
HyunSik Ryu <hsryu123@naver.com>
2023-04-03 19:00:13 +0900

    add, edit 시 validation 적용

--- src/main/java/hello/membermanagement/web/MemberController.java
+++ src/main/java/hello/membermanagement/web/MemberController.java
@@ -2,20 +2,23 @@ package hello.membermanagement.web;
 
 import hello.membermanagement.domain.member.Member;
 import hello.membermanagement.domain.member.MemberRepository;
+import hello.membermanagement.web.validationform.MemberValidationEditForm;
+import hello.membermanagement.web.validationform.MemberValidationSaveForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/basic/members")
 @RequiredArgsConstructor
-public class BasicMemberController {
+public class MemberController {
     private final MemberRepository memberRepository;
 
     /**
@@ -49,18 +52,29 @@ public class BasicMemberController {
      * @return addForm.html 멤버 등록 폼
      */
     @GetMapping("/add")
-    public String addForm(){
+    public String addForm(Model model){
+        model.addAttribute("member", new Member());
         return "basic/addForm";
     }
 
     /**
      * addForm에서 정보를 입력한 후 '멤버 등록' 버튼을 누르면 Post방식으로 submit 된다.
-     * @param member -> @ModelAttribute로 정보를 바탕으로 Member 객체를 생성한다.
+     * @param form -> @ModelAttribute로 정보를 바탕으로 form 객체를 생성한다.
+     * 이후 form의 속성값을 바탕으로 member 객체를 생성해준다.
      * @param redirectAttributes -> status=true 쿼리가 들어오면 상단의 텍스트를 변경하도록 member.html에 설정되어있다.
      * @return redirect 방식으로 /basic/members/{memberId}
      */
     @PostMapping("/add")
-    public String addMember(@ModelAttribute Member member, RedirectAttributes redirectAttributes){
+    public String addMember(@Validated @ModelAttribute("member") MemberValidationSaveForm form, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            return "basic/addForm"; // 입력 폼 템플릿
+        }
+
+        Member member = new Member(form.getName(),form.getAge(),
+                form.getBirthday(),form.getPhoneNumber(),form.getEmail());
+
         Member savedMember = memberRepository.save(member);
         redirectAttributes.addAttribute("memberId",savedMember.getId());
         redirectAttributes.addAttribute("status", true);
@@ -87,7 +101,16 @@ public class BasicMemberController {
      * @return redirect 방식으로 수정된 정보가 적용된 /members/{memberId} 로 돌아간다.
      */
     @PostMapping("/{memberId}/edit")
-    public String edit(@PathVariable Long memberId, @ModelAttribute Member member){
+    public String edit(@PathVariable Long memberId, @Validated @ModelAttribute("member") MemberValidationEditForm form, BindingResult bindingResult){
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            return "basic/editForm"; // 입력 폼 템플릿
+        }
+
+        Member member = new Member(form.getName(),form.getAge(),
+                form.getBirthday(),form.getPhoneNumber(),form.getEmail());
+
         memberRepository.updateMember(memberId,member);
         return "redirect:/basic/members/{memberId}";
     }
@@ -97,7 +120,8 @@ public class BasicMemberController {
      * @return deleteForm을 뿌려준다.
      */
     @GetMapping("/delete")
-    public String deleteForm(){
+    public String deleteForm(Model model){
+        model.addAttribute("member", new Member());
         return "basic/deleteForm";
     }
 
@@ -121,8 +145,8 @@ public class BasicMemberController {
      */
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("memberA", 20, "19981212", "01023234545","abc123@naver.com"));
-        memberRepository.save(new Member("memberB", 30, "19941125", "01026734812","hello@gmail.com"));
+        memberRepository.save(new Member("memberA", 20, "19981212", "010-2323-4545","abc123@naver.com"));
+        memberRepository.save(new Member("memberB", 30, "19941125", "010-2673-4812","hello@gmail.com"));
     }
 
 }
---
+++ src/main/java/hello/membermanagement/web/validationform/MemberValidationSaveForm.java
@@ -0,0 +1,30 @@
+package hello.membermanagement.web.validationform;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Data
+public class MemberValidationSaveForm {
+
+    @NotBlank
+    @Pattern(regexp = "^[가-힣]*$|^[a-zA-Z]*$")
+    private String name;
+    @NotNull
+    @Range(min = 1, max = 120)
+    private Integer age;
+
+
+    @Pattern(regexp = "\\d{4}\\d{2}\\d{2}")
+    private String birthday;
+
+    @NotNull
+    @Pattern(regexp = "(02|010)-\\d{3,4}-\\d{4}")
+    private String phoneNumber;
+    @Email
+    private String email;
+}
